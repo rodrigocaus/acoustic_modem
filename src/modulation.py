c66d4ab781db3c7f9e280530f10b46f91bef1571
Rodrigo Caus <digocaus@hotmail.com>
2019-11-01 00:42:32 -0300

    Adicionado código de exemplo de modulação

---
+++ src/modulation.py
@@ -0,0 +1,41 @@
+'''
+    Script for FSK modulation
+        Frequêncy modulation based on digital or binary information
+
+'''
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+def randomBitArray(n):
+    return np.random.randint(0,2,n)
+
+def bitsToWave(bits, Fs=44100, baud=10):
+    sample_bit = int(Fs/baud)
+    wave = np.repeat(bits, sample_bit)
+    wave = 2.0 * wave - 1.0
+    return wave
+
+def FSKMod(bitwave, Fs = 44100, f0 = 1500.0, df = 500.0):
+    time_end = len(bitwave)/Fs
+    t = np.linspace(0.0, time_end, len(bitwave))
+    fsk = np.sin(2.0*np.pi*(f0*t + df*bitwave*t))
+    return [t, fsk]
+
+bits = randomBitArray(10)
+mb = bitsToWave(bits)
+t, x = FSKMod(mb, f0=30.0, df=10.0)
+
+## Plotting Data ##
+ax_bit = plt.subplot(211)
+ax_bit.plot(t, mb)
+ax_bit.set_ylabel('Mensagem binária')
+ax_bit.grid(True)
+
+ax_mod = plt.subplot(212)
+ax_mod.plot(t, x)
+ax_mod.set_ylabel('Mensagem modulada')
+ax_mod.set_xlabel('Tempo (s)')
+ax_mod.grid(True)
+
+plt.show()
