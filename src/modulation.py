fe529d9a85adc3a6c2a4115b9d31be60ebcfbc1d
Rodrigo Caus <digocaus@hotmail.com>
2019-11-01 02:03:30 -0300

    Pequena alteração de otimização na modulação

--- src/modulation.py
+++ src/modulation.py
@@ -19,7 +19,7 @@ def bitsToWave(bits, Fs=44100, baud=10):
 def FSKMod(bitwave, Fs = 44100, f0 = 1500.0, df = 500.0):
     time_end = len(bitwave)/Fs
     t = np.linspace(0.0, time_end, len(bitwave))
-    fsk = np.sin(2.0*np.pi*(f0*t + df*bitwave*t))
+    fsk = np.sin(2.0*np.pi*(f0 + df*bitwave)*t)
     return [t, fsk]
 
 bits = randomBitArray(10)
